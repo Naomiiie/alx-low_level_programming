471f75e15709d5ce59364b2789af011a2d9b9e61
Naomiiie <naomionyancha123@gmail.com>
2023-07-09 09:08:24 -0700

    ./mynewnameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main - prints its name, followed by a new line.
@@ -8,6 +9,6 @@
 int main(int argc, char *argv[])
 {
 	if (argc >= 1)
-	printf("File name is %s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
